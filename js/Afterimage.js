0cc13c17158c8b784dd7ba6f788690c5b7126f45
truemaxdh <truemaxdh@gmail.com>
2020-06-20 22:58:27 +0900

    Update Afterimage.js

--- js/Afterimage.js
+++ js/Afterimage.js
@@ -34,6 +34,7 @@ specialEffects.afterimage.drawFrm = function() {
   // ---------
   // bg canvas
   // ---------
+  ctx_bg.globalCompositeOperation = 'source-over';
   ctx_bg.fillStyle = "rgba(0, 0, 0, 0.05)";
   ctx_bg.fillRect(0, 0, obj.w, obj.h);
 
