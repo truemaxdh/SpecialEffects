5beed1de868a0ce163e90bf981e679d0954c2e4a
truemaxdh <truemaxdh@gmail.com>
2020-12-22 01:57:54 +0900

    Update Pointillism.js

--- js/Pointillism.js
+++ js/Pointillism.js
@@ -2,14 +2,6 @@ if (typeof specialEffects === 'undefined' || !specialEffects) {
   specialEffects = {};
 }
 
-// snow
-function _snow(cx, cy, r, dy) {
-  this.cx = cx;
-  this.cy = cy;
-  this.r = r;
-  this.dy = dy;
-}
-
 specialEffects.pointillism = function(el) {
   console.log(el.style);
   
@@ -26,39 +18,39 @@ specialEffects.pointillism = function(el) {
   obj.ctx = cnv.getContext("2d");
   obj.w = cnv.width;
   obj.h =cnv.height;
+  obj.lastTimeStamp = null;
   obj.imgOri = new Image();
   obj.imgOri.src = "images/20170727_130136.jpg";
-  obj.lastTimeStamp = null;
   
+  obj.drawFrm = function(timeStamp) {
+    if (!obj.lastTimeStamp) obj.lastTimeStamp = timeStamp;
+    if ((timeStamp - obj.lastTimeStamp) > 1000) {
+      obj.lastTimeStamp = timeStamp;
+
+      // clear
+      obj.ctx.globalCompositeOperation = "source-over";
+      obj.ctx.beginPath();
+      obj.ctx.fillStyle="black";
+      obj.ctx.rect(0, 0, obj.w, obj.h);
+      obj.ctx.fill();
+
+      // draw
+      obj.ctx.drawImage(obj.imgOri, 0, 0);
+      obj.ctx.beginPath();
+      var d = Math.min(obj.imgOri.width, obj.imgOri.height) / 50;
+      for (var y = d; y < obj.imgOri.height; y+=d) {
+        for (var x = d; x < obj.imgOri.width; x+=d) {
+          var color = obj.ctx.getImageData(x, y, 1, 1);
+          obj.ctx.fillStyle="rgba(" + color[0] + "," + color[1] + "," + color[2] + "," + color[3] + ")";
+          obj.ctx.rect(x - d / 2, y - d / 2, d, d);       
+        }
+      }
+      obj.ctx.fill();
+    }
+
+    requestAnimationFrame(obj.drawFrm);
+  }
   obj.drawFrm();
 }
 
-specialEffects.pointillism.drawFrm = function(timeStamp) {
-  var obj = this.pointillism;
-  if (!obj.lastTimeStamp) obj.lastTimeStamp = timeStamp;
-  if ((timeStamp - obj.lastTimeStamp) > 1000) {
-    obj.lastTimeStamp = timeStamp;
 
-    // clear
-    obj.ctx.globalCompositeOperation = "source-over";
-    obj.ctx.beginPath();
-    obj.ctx.fillStyle="black";
-    obj.ctx.rect(0, 0, obj.w, obj.h);
-    obj.ctx.fill();
-
-    // draw
-    obj.ctx.drawImage(obj.imgOri, 0, 0);
-    obj.ctx.beginPath();
-    var d = Math.min(obj.imgOri.width, obj.imgOri.height) / 50;
-    for (var y = d; y < obj.imgOri.height; y+=d) {
-      for (var x = d; x < obj.imgOri.width; x+=d) {
-        var color = obj.ctx.getImageData(x, y, 1, 1);
-        obj.ctx.fillStyle="rgba(" + color[0] + "," + color[1] + "," + color[2] + "," + color[3] + ")";
-        obj.ctx.rect(x - d / 2, y - d / 2, d, d);       
-      }
-    }
-    obj.ctx.fill();
-  }
-
-  requestAnimationFrame(obj.drawFrm);
-}
